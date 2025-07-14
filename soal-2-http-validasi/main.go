ef6b37571237dc6eec6aebf4c941c398c12871be
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2025-07-14 08:56:43 +0700

    feat(soal-2): Validasi field kueri email dan age
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- soal-2-http-validasi/main.go
+++ soal-2-http-validasi/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"net/mail"
+	"strconv"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 )
@@ -10,6 +13,11 @@ import (
 func sendResponseWithCode(w http.ResponseWriter, statusCode int, msg []byte) (int, error) {
 	w.WriteHeader(statusCode)
 	return w.Write(msg)
+}
+
+func isEmailValid(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
 }
 
 func main() {
@@ -38,17 +46,47 @@ func main() {
 		w.Header().Add("Content-Type", "application/json")
 
 		response := make(map[string]any)
-		response["ok"] = true
+
+		var errors []string
+		query := r.URL.Query()
+		status := http.StatusOK
+
+		email := strings.TrimSpace(query.Get("email"))
+		ageStr := strings.TrimSpace(query.Get("age"))
+		if len(email) == 0 {
+			errors = append(errors, "Field email tidak boleh kosong")
+		}
+		if len(ageStr) == 0 {
+			errors = append(errors, "Field age tidak boleh kosong")
+		}
+		if !isEmailValid(email) {
+			errors = append(errors, "Field email bukan email yang valid")
+		}
+
+		age, err := strconv.Atoi(ageStr)
+		if err != nil {
+			errors = append(errors, "Field age bukan angka yang valid")
+		}
+
+		logrus.Infof("Didapat email=%s dan age=%d", email, age)
+		logrus.Infof("Permintaan terdapat %d kesalahan kueri", len(errors))
+
+		if len(errors) > 0 {
+			response["errors"] = errors
+			status = http.StatusBadRequest
+		} else {
+			response["status"] = "ok"
+		}
 
 		marshal, err := json.Marshal(response)
 		if err != nil {
 			logrus.WithError(err).Error("Failed to marshal response")
 
-			sendResponseWithCode(w, http.StatusInternalServerError, []byte("Internal Server Error"))
+			sendResponseWithCode(w, http.StatusInternalServerError, []byte("{\"error\":\"Internal Server Error\"}"))
 			return
 		}
 
-		sendResponseWithCode(w, 200, marshal)
+		sendResponseWithCode(w, status, marshal)
 	})
 
 	http.ListenAndServe(":8080", nil)
